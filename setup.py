cd2d29e00c59d39d3d6c5c1d4fc90d7d06fd654f
Vera <vilina.develop@gmail.com>
2024-02-07 21:04:21 +0100

    Project setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup_params = dict(
+    name="network-coverage-api",
+    version="0.1",
+    description="Network coverage API",
+    url="https://https://github.com/verailina/network_coverage_api",
+    author="Vera Ilina",
+    author_email="vilina.develop@gmail.com",
+    keywords="api",
+    packages=find_packages(exclude=["unit_tests"]),
+    # python_requires=[],
+    install_requires=[],  # Optional
+    extras_require={
+        "test": ["pytest"],
+    }
+)
+
+if __name__ == "__main__":
+    setup(**setup_params)
