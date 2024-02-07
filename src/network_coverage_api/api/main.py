279502f34a4065605fa73111f46f04bf380a251a
Vera <vilina.develop@gmail.com>
2024-02-07 21:48:03 +0100

    Integrated with uvicorn

--- src/network_coverage_api/api/main.py
+++ src/network_coverage_api/api/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +12,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "network_coverage_api.api.main:app", host="127.0.0.1", port=8088, log_level="info"
+    )
