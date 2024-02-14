564bae9cc144ce8f7ba283044d1be1172ae1765f
Vera <vilina.develop@gmail.com>
2024-02-14 10:58:46 +0100

    Updated the workflow

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 from setuptools import setup, find_packages
-from os import path
-from io import open
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
 
 setup_params = dict(
     name="network_coverage_api",
@@ -21,8 +10,6 @@ setup_params = dict(
     keywords="network_coverage_api",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    # python_requires=[],
-    install_requires=[],  # Optional
     extras_require={
         "test": ["pytest"],
     },
